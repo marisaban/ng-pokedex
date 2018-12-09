8368943a29ed5fb84664a56046d8eef5c0805f75
Maria Isabel Bances <marisaban7@gmail.com>
2018-12-08 22:06:32 -0800

    added sass and bootstrap, created foundation for pokedex.

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -4,18 +4,31 @@ import { PokemonApiService } from '../service/pokemon-api.service';
 @Component({
   selector: 'app-pokedex',
   templateUrl: './pokedex.component.html',
-  styleUrls: ['./pokedex.component.css']
+  styleUrls: ['./pokedex.component.scss']
 })
 export class PokedexComponent implements OnInit {
+
+  pokemons = [];
+  myArray = [];
 
   constructor(private pokemonApi: PokemonApiService) { }
 
   ngOnInit() {
     this.pokemonApi.getPokemon()
-      .subscribe(data => {
-        console.log(data);
-        }
-      )
+    .subscribe(data => {
+      const pokeObj = data.results;
+        pokeObj.forEach(pokemon => {
+          this.pokemons = this.makeArray(pokemon);
+          console.log(pokemon.name);
+        })
+      }
+    )
+  }
+
+  makeArray(pokemon) {
+    pokemon._pokemonName = pokemon;
+    this.myArray.push(pokemon._pokemonName);
+    return this.myArray;
   }
 
 }
